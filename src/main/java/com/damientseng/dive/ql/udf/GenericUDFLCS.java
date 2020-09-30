096bd2bf85dcf56f965ba2d5c0427cf6491573c9
damientseng <acetseng@hotmail.com>
2020-09-30 16:49:52 +0800

    feat: longest common sub-sequence

---
+++ src/main/java/com/damientseng/dive/ql/udf/GenericUDFLCS.java
@@ -0,0 +1,74 @@
+package com.damientseng.dive.ql.udf;
+
+import org.apache.commons.text.similarity.LongestCommonSubsequence;
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.io.IntWritable;
+
+import static org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils.PrimitiveGrouping.STRING_GROUP;
+import static org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils.PrimitiveGrouping.VOID_GROUP;
+
+
+/**
+ * GenericUDFLCS
+ */
+@Description(name = "lcs", value = "_FUNC_(str1, str2) - "
+        + "Calculate the longest common sub-sequence of str1 and str2"
+        + "Example:\n "
+        + " > SELECT _FUNC_('abcde', 'aced');\n 3")
+public class GenericUDFLCS extends GenericUDF {
+    private final IntWritable output = new IntWritable();
+    private transient ObjectInspectorConverters.Converter[] converters = new ObjectInspectorConverters.Converter[2];
+    private transient PrimitiveObjectInspector.PrimitiveCategory[] inputTypes = new PrimitiveObjectInspector.PrimitiveCategory[2];
+    private transient LongestCommonSubsequence lcsUtil;
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
+        checkArgsSize(arguments, 2, 2);
+
+        checkArgPrimitive(arguments, 0);
+        checkArgPrimitive(arguments, 1);
+
+        checkArgGroups(arguments, 0, inputTypes, STRING_GROUP, VOID_GROUP);
+        checkArgGroups(arguments, 1, inputTypes, STRING_GROUP, VOID_GROUP);
+
+        obtainStringConverter(arguments, 0, inputTypes, converters);
+        obtainStringConverter(arguments, 1, inputTypes, converters);
+
+        lcsUtil = new LongestCommonSubsequence();
+
+        return PrimitiveObjectInspectorFactory.writableIntObjectInspector;
+    }
+
+    @Override
+    public Object evaluate(DeferredObject[] arguments) throws HiveException {
+        String str0 = getStringValue(arguments, 0, converters);
+        String str1 = getStringValue(arguments, 1, converters);
+
+        if (str0 == null || str1 == null) {
+            return null;
+        }
+
+        int length = lcsUtil.apply(str0, str1);
+        output.set(length);
+        return output;
+    }
+
+    @Override
+    public String getDisplayString(String[] children) {
+        return getStandardDisplayString(getFuncName(), children);
+    }
+
+    @Override
+    protected String getFuncName() {
+        return "lcs";
+    }
+
+}
+
---
+++ src/test/java/com/damientseng/dive/ql/udf/GenericUDFLCSTest.java
@@ -0,0 +1,38 @@
+package com.damientseng.dive.ql.udf;
+
+import junit.framework.TestCase;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+
+public class GenericUDFLCSTest extends TestCase {
+
+    public void testLCS() throws HiveException {
+        GenericUDFLCS udf = new GenericUDFLCS();
+        ObjectInspector valueOI0 = PrimitiveObjectInspectorFactory.writableStringObjectInspector;
+        ObjectInspector valueOI1 = PrimitiveObjectInspectorFactory.writableStringObjectInspector;
+        ObjectInspector[] arguments = {valueOI0, valueOI1};
+
+        udf.initialize(arguments);
+        apply("abcde", "aced", 3, udf);
+        apply("abc", "abc", 3, udf);
+        apply("abc", "def", 0, udf);
+
+        apply("abc", "", 0, udf);
+        apply("", "abc", 0, udf);
+    }
+
+    private void apply(String str0, String str1, Integer expect, GenericUDF udf)
+            throws HiveException {
+        GenericUDF.DeferredObject valueObj0 = new GenericUDF.DeferredJavaObject(new Text(str0));
+        GenericUDF.DeferredObject valueObj1 = new GenericUDF.DeferredJavaObject(new Text(str1));
+        GenericUDF.DeferredObject[] args = {valueObj0, valueObj1};
+        IntWritable output = (IntWritable) udf.evaluate(args);
+        assertNotNull("lcs test", output);
+        assertEquals("lcs test", expect.intValue(), output.get());
+    }
+}
