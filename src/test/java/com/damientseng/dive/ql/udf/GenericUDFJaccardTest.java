f14b718ef4997b52087e94f5b0fc2f093ed74933
damientseng <acetseng@hotmail.com>
2020-10-01 14:39:48 +0800

    feat: jaccard similarity

---
+++ src/test/java/com/damientseng/dive/ql/udf/GenericUDFJaccardTest.java
@@ -0,0 +1,39 @@
+package com.damientseng.dive.ql.udf;
+
+import junit.framework.TestCase;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+
+
+public class GenericUDFJaccardTest extends TestCase {
+
+    public void testJaccard() throws HiveException {
+        GenericUDFJaccard udf = new GenericUDFJaccard();
+        ObjectInspector valueOI0 = PrimitiveObjectInspectorFactory.writableStringObjectInspector;
+        ObjectInspector valueOI1 = PrimitiveObjectInspectorFactory.writableStringObjectInspector;
+        ObjectInspector[] arguments = {valueOI0, valueOI1};
+
+        udf.initialize(arguments);
+        apply("abcde", "aced", 0.8d, udf);
+        apply("abc", "abc", 1d, udf);
+        apply("abc", "def", 0d, udf);
+
+        apply("abc", "", 0d, udf);
+        apply("", "abc", 0d, udf);
+    }
+
+    private void apply(String str0, String str1, Double expect, GenericUDF udf)
+            throws HiveException {
+        GenericUDF.DeferredObject valueObj0 = new GenericUDF.DeferredJavaObject(new Text(str0));
+        GenericUDF.DeferredObject valueObj1 = new GenericUDF.DeferredJavaObject(new Text(str1));
+        GenericUDF.DeferredObject[] args = {valueObj0, valueObj1};
+        DoubleWritable output = (DoubleWritable) udf.evaluate(args);
+        assertNotNull("jaccard test", output);
+        System.out.println(output.get());
+        assertEquals("jaccard test", expect, output.get());
+    }
+}
